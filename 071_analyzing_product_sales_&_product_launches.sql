99ff19449949b3e4a718a0dca4da55434abb10fc
pochiman <pochiman@yahoo.com>
2025-03-11 10:17:32 -0400

    initial commit

---
+++ 071_analyzing_product_sales_&_product_launches.sql
@@ -0,0 +1,14 @@
+USE mavenfuzzyfactory;
+
+
+SELECT
+	primary_product_id,
+    COUNT(order_id) AS orders,
+    SUM(price_usd) AS revenue,
+    SUM(price_usd - cogs_usd) AS margin,
+    AVG(price_usd) AS aov
+
+FROM orders
+WHERE order_id BETWEEN 10000 AND 11000 -- arbitrary
+GROUP BY 1
+ORDER BY 2 DESC;
