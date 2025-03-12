f5a4706f7f668f571ed8f63d948c65cf608ca2ed
pochiman <pochiman@yahoo.com>
2025-03-11 23:12:47 -0400

    initial commit

---
+++ 096_analyzing_repeat_channel_behavior.sql
@@ -0,0 +1,35 @@
+USE mavenfuzzyfactory;
+
+SELECT
+	utm_source,
+    utm_campaign,
+    http_referer,
+    COUNT(CASE WHEN is_repeat_session = 0 THEN website_session_id ELSE NULL END) AS new_sessions,
+    COUNT(CASE WHEN is_repeat_session = 1 THEN website_session_id ELSE NULL END) AS repeat_sessions
+FROM website_sessions
+WHERE created_at < '2014-11-05' -- the date of the assignment
+	AND created_at >= '2014-01-01' -- prescribed date range in assignment
+GROUP BY 1, 2, 3
+ORDER BY 5 DESC;
+
+
+
+
+SELECT
+	CASE
+		WHEN utm_source IS NULL AND http_referer IN ('https://www.gsearch.com', 'https://www.bsearch.com') THEN 'organic_search'
+		WHEN utm_campaign = 'nonbrand' THEN 'paid_nonbrand'
+        WHEN utm_campaign = 'brand' THEN 'paid_brand'
+        WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
+        WHEN utm_source = 'socialbook' THEN 'paid_social'
+	END AS channel_group,
+    -- utm_source,
+    -- utm_campaign,
+    -- http_referer,
+    COUNT(CASE WHEN is_repeat_session = 0 THEN website_session_id ELSE NULL END) AS new_sessions,
+    COUNT(CASE WHEN is_repeat_session = 1 THEN website_session_id ELSE NULL END) AS repeat_sessions
+FROM website_sessions
+WHERE created_at < '2014-11-05' -- the date of the assignment
+	AND created_at >= '2014-01-01' -- prescribed date range in assignment
+GROUP BY 1
+ORDER BY 3 DESC;
